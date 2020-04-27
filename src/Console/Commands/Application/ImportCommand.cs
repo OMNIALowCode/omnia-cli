2c74b76c39a74ed52a8de3e48ecba4dab2a13524
brunoCo3ta <59695845+brunoCo3ta@users.noreply.github.com>
2020-04-27 14:53:54 +0100

    Add NPOI to read excel

--- src/Console/Commands/Application/ImportCommand.cs
+++ src/Console/Commands/Application/ImportCommand.cs
@@ -1,6 +1,8 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using Omnia.CLI.Extensions;
 using System;
 using System.Globalization;
@@ -17,6 +19,7 @@ namespace Omnia.CLI.Commands.Application
     {
         private readonly AppSettings _settings;
         private readonly HttpClient _httpClient;
+
         public ImportCommand(IOptions<AppSettings> options, IHttpClientFactory httpClientFactory)
         {
             _settings = options.Value;
@@ -25,10 +28,13 @@ namespace Omnia.CLI.Commands.Application
 
         [Option("--subscription", CommandOptionType.SingleValue, Description = "Name of the configured subscription.")]
         public string Subscription { get; set; }
+
         [Option("--tenant", CommandOptionType.SingleValue, Description = "Tenant to export.")]
         public string Tenant { get; set; }
+
         [Option("--environment", CommandOptionType.SingleValue, Description = "Environment to export.")]
         public string Environment { get; set; } = Constants.DefaultEnvironment;
+
         [Option("--path", CommandOptionType.SingleValue, Description = "Complete path to the file.")]
         public string Path { get; set; }
 
@@ -50,12 +56,46 @@ namespace Omnia.CLI.Commands.Application
 
             await _httpClient.WithSubscription(sourceSettings);
 
-            // TODO
-
+            /* TODO
+                    GET File
+                    Reed Sheets
+                    Get Entity
+                    Read Headers
+                    Read Rows
+                    Compose Json
+                    Call API
+            */
             Console.WriteLine($"Successfully imported data to tenant \"{Tenant}\".");
             return (int)StatusCodes.Success;
         }
 
+        private static void ReadExcel(string path)
+        {
+            var workbook = new XSSFWorkbook(path);
+
+            Console.WriteLine("NumberOfSheets:{0}", workbook.NumberOfSheets);
+
+            for (int s = 0; s < workbook.NumberOfSheets; s++)
+            {
+                var sheet = workbook.GetSheetAt(s);
+
+                string entityName = sheet.SheetName;
+
+                Console.WriteLine("entityName:{0}", entityName);
+
+                Console.WriteLine("PhysicalNumberOfRows:{0}", sheet.PhysicalNumberOfRows);
+
+                for (int rownum = 0; rownum < sheet.PhysicalNumberOfRows; rownum++)
+                {
+                    var row = sheet.GetRow(rownum);
+
+                    foreach (var cell in row.Cells)
+                    {
+                        Console.WriteLine("Cell:{0}", cell.StringCellValue);
+                    }
+                }
+            }
+        }
 
         private class ApiError
         {
